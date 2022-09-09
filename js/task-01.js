daaf5d27489baa78b7c7a902e627c4ae0d11ba44
Mykola Berezhniuk <bermukola@gmail.com>
2022-09-09 23:48:02 +0300

    Task-1 and Task-2

---
+++ js/task-01.js
@@ -0,0 +1,33 @@
+// Ex. 1 - Task 1 ---------------------------------------------------------
+
+const arrayCategories = document.querySelectorAll('li.item');
+
+function getLengthNumberOfCategories() {
+  return arrayCategories.length;
+}
+
+console.log('Number of categories:', getLengthNumberOfCategories());
+
+// Ex. 1 - Task 2 ----------------------------------------------------------
+
+function getNameCategoryAndNumberOfElements(array) {
+  // Through FOR..OF
+  //   for (const item of arrayCategories) {
+  //     let nameEl = item.querySelector('h2').textContent;
+  //     let numberElements = item.querySelectorAll('li').length;
+
+  //     console.log('Category:', nameEl);
+  //     console.log('Elements:', numberElements);
+  //   }
+
+  // Through Map()
+  let test = Array.from(array).map(item => {
+    let nameEl = item.querySelector('h2').textContent;
+    let numberElements = item.querySelectorAll('li').length;
+
+    console.log('Category:', nameEl);
+    console.log('Elements:', numberElements);
+  });
+}
+
+getNameCategoryAndNumberOfElements(arrayCategories);
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,26 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+// Create empty array
+const itemArray = [];
+
+// Function for create items
+function getListItems(ingredients) {
+  for (const item of ingredients) {
+    let itemEl = document.createElement('li');
+    itemEl.textContent = item;
+    itemEl.classList.add('item');
+
+    itemArray.push(itemEl);
+  }
+
+  return itemArray;
+}
+
+// Get link for element
+const ingredientsEl = document.querySelector('#ingredients');
+
+// Call function
+getListItems(ingredients);
+
+// Add to HTML
+ingredientsEl.append(...itemArray);

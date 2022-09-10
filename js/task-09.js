cfee4bbd417a3bf72e02742268943e6d699d268a
Mykola Berezhniuk <bermukola@gmail.com>
2022-09-10 17:39:56 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+// Getting Ref
+const bodyEl = document.querySelector('body');
+const btnEl = document.querySelector('button.change-color');
+const textColorEl = document.querySelector('span.color');
+
+btnEl.addEventListener('click', () => {
+  let randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  textColorEl.textContent = `${randomColor}`;
+});
